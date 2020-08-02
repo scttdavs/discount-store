e70f489704f78b52ea5a43dd53d774f5b6460dde
Scott Davis <sc.ttdav.s@gmail.com>
2020-08-01 22:25:12 -0400

    adjust types

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+type Callback = (...any) => void;
 type Callbacks = {
-    get: { (value: any): any; } [],
-    set: { (key: string, value: any): any; } [],
-    clear: { (): any; } [],
-    reset: { (): any; } []
+    get: Callback[],
+    set: Callback[],
+    clear: Callback[],
+    reset: Callback[]
 }
 
 type State = Record<string, any>
-type Callback = (...any) => void;
 type StoreEventNames = 'get' | 'set' | 'clear' | 'reset';
 type OnMethod = (eventName: StoreEventNames, callback: Callback) => () => void
-type OnChangeMethod = (propName: string, callback: (value?: any) => void) => void
+type OnChangeMethod = (propName: string, callback: (value?: any) => void) => () => void
 type GetMethod = (key: string) => any
 type SetMethod = (key: string, value: any) => any
 type ResetMethod = () => void;
